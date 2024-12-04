493ca9ad3fdbfc70c90d4aabc61e30bcf612c216
sonuray576 <sonuray576@gmail.com>
2024-12-04 21:50:45 +0530

    Add files via upload

---
+++ config/constant.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    "roles"=>[
+        ['name'=>'Admin'],
+        ['name'=>'Manager'],
+        ['name'=>'User']
+    ],
+
+    "permissions"=>[
+        ['name'=>'manage_users'],
+        ['name'=>'view_reports'],
+        ['name'=>'edit_profile']
+    ]
+];
